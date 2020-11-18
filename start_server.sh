98c7d59330eb1302b0323be9dd3be16c9fd61751
SeeLog <seelog693@gmail.com>
2020-11-18 17:18:08 +0900

    [add] 起動スクリプトからport指定できるようにした

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+
+PORT=5000
+
+function usage {
+    cat <<EOM
+Usage: $(basename "$0") [OPTION]...
+    -p, --port      ポート番号を指定
+    -h, --help      このヘルプを表示
+EOM
+    exit 2
+}
+
+while getopts ":p:h" optKey; do
+    case "$optKey" in
+        p|'--port')
+            PORT=${OPTARG}
+            ;;
+        '-h'|'--help'|* )
+        usage
+        ;;
+    esac
+done
 
 cd app
-python -m persona_chatbot_app.bot.server
+python -m persona_chatbot_app.bot.server -p ${PORT}
