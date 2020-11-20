24a7d7f8de7ee80a4af3ec67e6d417d5c8330f82
SeeLog <seelog693@gmail.com>
2020-11-21 03:32:25 +0900

    [change] Pythonで鯖立てするよとわかり易い名前にした

---
+++ start_server_python.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PORT=5000
+IP=0.0.0.0
+
+function usage {
+    cat <<EOM
+Usage: $(basename "$0") [OPTION]...
+    -p              ポート番号を指定
+    -i              IPまたはHostを指定
+    -h, --help      このヘルプを表示
+EOM
+    exit 2
+}
+
+while getopts ":p:h" optKey; do
+    case "$optKey" in
+        p)
+            PORT=${OPTARG}
+            ;;
+        i)
+            IP=${OPTARG}
+            ;;
+        '-h'|'--help'|* )
+        usage
+        ;;
+    esac
+done
+
+cd app
+pipenv run python -m persona_chatbot_app.bot.server -p ${PORT} -i ${IP}
